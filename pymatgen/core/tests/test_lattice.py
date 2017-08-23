698721ffb3cea0c5d012f1d0377c93d2493c3a38
Kemper <tkemper@tkemper-23195s.nrel.gov>
2017-08-22 18:13:54 -0600

    Update tests for lattice

--- pymatgen/core/tests/test_lattice.py
+++ pymatgen/core/tests/test_lattice.py
@@ -453,6 +453,35 @@ class LatticeTestCase(unittest.TestCase):
                                     l2_fcoords)
 
 
+class TestLattice(unittest.TestCase):
+    def setUp(self):
+        # fixture 
+        matrix = [ 100,0,0,0,100,0,0,0,100 ]
+        self.lat = Lattice(matrix)
+        
+        self.assertEqual(self.lat._lengths[0],100.0)
+        self.assertEqual(self.lat._lengths[1],100.0)
+        self.assertEqual(self.lat._lengths[2],100.0)
+
+        self.assertEqual(self.lat._angles[0],90.0)
+        self.assertEqual(self.lat._angles[1],90.0)
+        self.assertEqual(self.lat._angles[2],90.0)
+        
+    def test_changematrix(self):
+        matrix = [ 132,0,0,0,127,0,0,0,150 ]
+        self.lat.set_matrix(matrix)
+        self.assertEqual(self.lat._lengths[0],132.0)
+        self.assertEqual(self.lat._lengths[1],127.0)
+        self.assertEqual(self.lat._lengths[2],150.0)
+
+        self.assertEqual(self.lat._angles[0],90.0)
+        self.assertEqual(self.lat._angles[1],90.0)
+        self.assertEqual(self.lat._angles[2],90.0)
+                
+    def tearDown(self):
+        del self.lat 
+        self.lat = None
+        
 if __name__ == '__main__':
     import unittest
     unittest.main()
